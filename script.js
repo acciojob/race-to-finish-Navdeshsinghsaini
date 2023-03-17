7cf43f0d5c4e61b9d16a60b9e3450ebbb77ea123
Navdeshsinghsaini <naveshsinghsaini@gmail.com>
2023-03-17 11:27:39 +0530

    second1 commit

--- script.js
+++ script.js
@@ -1,4 +1,41 @@
-window.promises = [];
+let show = document.getElementById("output");
+// window.promises = [promise1 , promise2 ,promise3 ,promise4 , promise5];
 
+let promise1 = new Promise( (resolve , reject) => {
+    setTimeout(() => {
+        resolve("hello 1")
+    }, 4000)
+})
+
+let promise2 = new Promise( (resolve , reject) => {
+    setTimeout(() => {
+        resolve("hello 2")
+    }, 5000)
+})
+
+let promise3 = new Promise( (resolve , reject) => {
+    setTimeout(() => {
+        resolve("hello 3")
+    }, 1000)
+})
+
+let promise4 = new Promise( (resolve , reject) => {
+    setTimeout(() => {
+        resolve("hello 4")
+    }, 2000)
+})
+
+let promise5 = new Promise( (resolve , reject) => {
+    setTimeout(() => {
+        resolve("hello 5")
+    }, 3000)
+})
+
+
+window.promises = [promise1 , promise2 ,promise3 ,promise4 , promise5];
 // Do not change the code above this
 // add your promises to the array `promises`
+Promise.any([promise1 , promise2 ,promise3 ,promise4 , promise5])
+.then((res) => {
+    show.innerText = res ;
+})
